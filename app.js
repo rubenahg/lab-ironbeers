5cbb0d8176834b673eeaa6d8ac9ce4f2d9a5a383
Ruben Dietl <ruben.dietl@adamhall.com>
2023-10-07 17:25:18 +0200

    Lab solved (with bonus :)

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, "views/partials"))
 
 // Add the route handlers here:
 
@@ -22,4 +22,73 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', async (req, res) => {
+  try {
+    let beers = await punkAPI.getBeers()
+    let beersData = beers.map((beers)=>{
+      return {
+        beerName: beers.name,
+        beerImage: beers.image_url,
+        beerDescription: beers.description,
+        beerTagline: beers.tagline,
+        beerID: beers.id
+      }
+    })
+    res.render('beers', {beersData});
+  } catch (error) {
+    console.log("something went wrong: ", error)
+  }
+});
+
+app.get('/random-beer', async (req, res) => {
+  try {
+    let getBeer = await punkAPI.getRandom()
+    let randomBeerData = getBeer[0];
+
+    let beer = {
+      beerName: randomBeerData.name,
+      beerImage: randomBeerData.image_url,
+      beerDescription: randomBeerData.description,
+      beerTagline: randomBeerData.tagline,
+      beerFoodPairing: randomBeerData.food_pairing,
+      beerBrewersTips: randomBeerData.brewers_tips
+    };
+    res.render('random-beers', {beer});
+  } catch (error) {
+    console.log("something went wrong: ", error)
+  }
+});
+
+app.get('/beer/:beerID', async (req, res) => {
+  try {
+    let beers = await punkAPI.getBeers()
+    let reqID = parseInt(req.params.beerID)
+    let theBeer = checkForBeer(beers, reqID)
+    let beer = {
+      beerName: theBeer.name,
+      beerImage: theBeer.image_url,
+      beerDescription: theBeer.description,
+      beerTagline: theBeer.tagline,
+      beerFoodPairing: theBeer.food_pairing,
+      beerBrewersTips: theBeer.brewers_tips
+    };
+    res.render('beer', {beer});
+  } catch (error) {
+    console.log("something went wrong: ", error)
+  }
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
+
+// check for beer req URL Match
+
+function checkForBeer(beers, reqID){
+  for (let i = 0; i < beers.length; i++) {
+    if(beers[i].id === reqID){
+      return beers[i]
+      break;
+    }
+  }
+}
